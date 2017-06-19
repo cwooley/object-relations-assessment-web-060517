e60fc464f960382422acec905cefea53481d2d1b
github username <github email address>
2017-06-19 09:59:42 -0400

    Finished Assessment

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,42 @@
 class Customer
   attr_accessor :first_name, :last_name
 
+  @@all = []
+
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    Customer.all.find do |customer|
+      name == customer.full_name
+    end
+  end
+
+  def self.find_all_by_first_name(first_name)
+    Customer.select do |customer|
+      customer.first_name == first_name
+    end
+  end
+
+  def self.all_names
+    @@all.map do |customer|
+      customer.full_name
+    end
+  end
+
+  def add_review(restaurant, content)
+    review = Review.new(self, restaurant, content)
+  end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,30 @@ def reload
   load 'config/environment.rb'
 end
 
+
+charles = Customer.new("Charles", "Wooley")
+luke = Customer.new("Lukasz", "Rus")
+eph = Customer.new("Ephraim", "Montesabian")
+josh = Customer.new("Josh", "Clevenger")
+tina = Customer.new("Tina", "Caracci")
+hannah = Customer.new("Hannah", "Fitzgibbons")
+
+
+surftaco = Restaurant.new("Surftaco")
+andys = Restaurant.new("Andys")
+asahi = Restaurant.new("Asahi")
+ruby = Restaurant.new("Ruby")
+
+charles.add_review(surftaco, "THIS PLACE ROCKS")
+charles.add_review(asahi, "Raw fish is the best fish")
+luke.add_review(andys, "Polish food is better")
+tina.add_review(surftaco, "MMM TACOS")
+eph.add_review(andys, "Pizza is delicious")
+josh.add_review(surftaco, "Tacos bro")
+hannah.add_review(asahi, "Sushi is tasty")
+
+
+
+
+
 Pry.start
